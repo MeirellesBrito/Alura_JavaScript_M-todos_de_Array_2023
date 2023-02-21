381bbfca3e8e6eeec652bcb780cfc02a037989c6
MeirellesBrito <aluno.f.meirelles@gmail.com>
2023-02-21 19:27:27 -0300

    Alura JS | Inicio do curso da Alura JavaScript: métodos de array | Fetch de livros no console  - 2023

---
+++ app/main.js
@@ -0,0 +1,9 @@
+let livros = []
+const endPointDaAPI = 'https://guilhermeonrails.github.io/casadocodigo/livros.json'
+getBuscarLIvrosDAAPI()
+
+async function getBuscarLIvrosDAAPI(){
+    const res = await fetch(endPointDaAPI)
+    livros = await res.json()
+    console.table(livros)
+};

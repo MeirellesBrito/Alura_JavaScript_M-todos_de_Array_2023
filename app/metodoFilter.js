f063117177f2c9cc2f8a368cc5a6d9d4936e64c7
MeirellesBrito <aluno.f.meirelles@gmail.com>
2023-02-22 02:41:00 -0300

    Alura JS | Inicio do curso da Alura JavaScript: métodos de array | Filtrando livros disponíveis  - 2023

--- app/metodoFilter.js
+++ app/metodoFilter.js
@@ -4,6 +4,6 @@ botoes.forEach(btn => btn.addEventListener('click', filtrarLivros))
 function filtrarLivros(){
     const elementoBtn = document.getElementById(this.id)
     const categoria = elementoBtn.value
-    let livrosFiltrados = livros.filter(livro => livro.categoria == categoria)
+    let livrosFiltrados = categoria == 'disponivel' ? livros.filter(livros => livros.quantidade > 0 > 0 ) : livros.filter(livros => livros.categoria == categoria)
     exibirOsLivrosNaTela(livrosFiltrados)
-}
+} 

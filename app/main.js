877238d5eacd1bcfe1ef0741011ba27178161f8d
MeirellesBrito <aluno.f.meirelles@gmail.com>
2023-02-21 23:12:58 -0300

    Alura JS | Inicio do curso da Alura JavaScript: métodos de array | Desconto com map  - 2023

--- app/main.js
+++ app/main.js
@@ -6,26 +6,10 @@ const elementoParaInserirLivros = document.getElementById('livros')
 async function getBuscarLIvrosDAAPI(){
     const res = await fetch(endPointDaAPI)
     livros = await res.json()
-    console.table(livros)
-    exibirOsLivrosNaTela(livros)
+    let livrosComDesconto = aplicarDesconto(livros)
+    exibirOsLivrosNaTela(livrosComDesconto)
 };
-function exibirOsLivrosNaTela(listaDeLivros){
-    listaDeLivros.forEach(livros => {
-        elementoParaInserirLivros.innerHTML += `
-        <div class="livro">
-      <img class="livro__imagens" src="${livros.imagem}" alt="${livros.alt}" />
-      <h2 class="livro__titulo">
-        ${livros.titulo}
-      </h2>
-      <p class="livro__descricao">${livros.autor}</p>
-      <p class="livro__preco" id="preco">${livros.preco}</p>
-      <div class="tags">
-        <span class="tag">${livros.categoria}/span>
-      </div>
-    </div>
-    `
-    })
-}
+
 
 
 

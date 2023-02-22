7b211297446f67af8f7293cbf3b9b32ec6c99c8e
MeirellesBrito <aluno.f.meirelles@gmail.com>
2023-02-21 22:17:20 -0300

    Alura JS | Inicio do curso da Alura JavaScript: métodos de array | forEach de livros  - 2023

--- app/main.js
+++ app/main.js
@@ -1,9 +1,40 @@
 let livros = []
 const endPointDaAPI = 'https://guilhermeonrails.github.io/casadocodigo/livros.json'
 getBuscarLIvrosDAAPI()
+const elementoParaInserirLivros = document.getElementById('livros')
 
 async function getBuscarLIvrosDAAPI(){
     const res = await fetch(endPointDaAPI)
     livros = await res.json()
     console.table(livros)
+    exibirOsLivrosNaTela(livros)
 };
+function exibirOsLivrosNaTela(listaDeLivros){
+    listaDeLivros.forEach(livros => {
+        elementoParaInserirLivros.innerHTML += `
+        <div class="livro">
+      <img class="livro__imagens" src="${livros.imagem}" alt="${livros.alt}" />
+      <h2 class="livro__titulo">
+        ${livros.titulo}
+      </h2>
+      <p class="livro__descricao">${livros.autor}</p>
+      <p class="livro__preco" id="preco">${livros.preco}</p>
+      <div class="tags">
+        <span class="tag">${livros.categoria}/span>
+      </div>
+    </div>
+    `
+    })
+}
+
+
+
+
+
+
+
+
+
+
+
+
